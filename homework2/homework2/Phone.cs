7287a7b43917b4275b65a306b97878241c80126a
ligaab <ligaabola4@gmail.com>
2022-04-25 19:22:19 +0300

    changes clas phone

---
+++ homework2/homework2/Phone.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework2
+{
+    public class Phone
+    {
+        public string Size { get; set; }
+        public string Brand { get; set; }
+        public string Mark { get; set; }
+        public void Call()
+        {
+            Console.WriteLine("Calling");
+        }
+        public void SendSms()
+        {
+            Console.WriteLine("SMS sent");
+        }
+    }
+}
---
+++ homework2/homework2/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+using homework2;
+
+Phone phone = new Phone();
+
+Console.WriteLine("Enter size mark model");
+String mark = Console.ReadLine();
+String size = Console.ReadLine();
+String model = Console.ReadLine();
+
+phone.Mark = mark;
+phone.Size = size;
+phone.Mark = model;
+
+
